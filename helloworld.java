7664b01855af23029d02c7d8d7c0a9e39f285858
umida1989 <82923156+umida1989@users.noreply.github.com>
2021-04-24 10:30:04 -0400

    Java Class in GitHub first day

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,5 @@
 public class Hello world 
 public static void main ( String []args){
-	System.out.println("Hello World")
+	System.out.println("Hello World");
+	System.out.println("Hello Classmates");
 }
